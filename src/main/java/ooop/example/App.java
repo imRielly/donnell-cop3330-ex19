4a18a07a0c0dcdfde6c8a7e08de04d5e047a58d6
imRielly <84353817+imRielly@users.noreply.github.com>
2021-06-06 21:37:38 -0400

    Initial commit

---
+++ src/main/java/ooop/example/App.java
@@ -0,0 +1,76 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Rielly Donnell
+ */
+
+package ooop.example;
+
+import java.util.Scanner;
+
+/**
+ Exercise 19 - BMI Calculator
+ You’ll often need to see if one value is within a certain range and alter the flow of a program as a result.
+
+ Create a program to calculate the body mass index (BMI) for a person using the person’s height
+ in inches and weight in pounds. The program should prompt the user for weight and height.
+
+ Calculate the BMI by using the following formula:
+
+ bmi = (weight / (height × height)) * 703
+ If the BMI is between 18.5 and 25, display that the person is at a normal weight.
+ If they are out of that range, tell them if they are underweight or overweight
+ and tell them to consult their doctor.
+
+ Example Output
+ Your BMI is 19.5.
+ You are within the ideal weight range.
+ or
+
+ Your BMI is 32.5.
+ You are overweight. You should see your doctor.
+
+ Constraint
+ Ensure your program takes only numeric data. Don’t let the user continue unless the data is valid.
+ Challenges
+ Make the user interface accept height and weight in Imperial or metric units. You’ll need a slightly different formula for metric units.
+ For Imperial measurements, prompt for feet and inches and convert feet to inches so the user doesn’t have to.
+ Use a GUI interface with sliders for height and weight. Update the user interface on the fly. Use colors as well as text to indicate health.
+
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        double weight = Double.parseDouble(requestNumber("Please enter your weight: "));
+        double height = Double.parseDouble(requestNumber("Please enter your height: "));
+        //(weight / (height × height)) * 703
+        double bmi = (weight / Math.pow(height, 2)) * 703;
+        System.out.println("Your BMI is " + bmi + ".");
+        if (bmi < 18.5) {
+            System.out.println("You're underweight. You need to see your doctor.");
+        } else if (bmi <= 25) {
+            System.out.println("Your weight is within the ideal range.");
+    } else {
+            System.out.println("You're overweight. You need to see your doctor.");
+        }
+    }
+
+    private static String requestNumber(String prompt){
+        Scanner in = new Scanner(System.in);
+
+        String input;
+
+        try {
+            System.out.print(prompt);
+            input = in.nextLine();
+            double testInput = Double.parseDouble(input); //Will intentionally throw error if not able to parse
+            if (testInput < 0) {
+                throw new NumberFormatException();
+            }
+        } catch (Exception e) {
+            input = requestNumber(prompt);
+        }
+
+        return input;
+    }
+}
